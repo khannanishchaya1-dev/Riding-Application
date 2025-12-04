771d77fc9679052f505ba32320eb690cdc4dd863
Nishchaya <khannanishchaya1@gmail.com>
2025-12-04 18:09:37 +0530

    .

--- Backend/app.js
+++ Backend/app.js
@@ -14,14 +14,15 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
