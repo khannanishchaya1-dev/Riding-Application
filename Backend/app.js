f9ef0f77c29ba59badfc8a29fe262835b57006ef
Nishchaya <khannanishchaya1@gmail.com>
2025-12-05 20:19:20 +0530

    cors policy issue

--- Backend/app.js
+++ Backend/app.js
@@ -14,15 +14,14 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "https://riding-application-x1x2.vercel.app", // your frontend
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // important
+}));
 
 app.use(express.json());
 app.use(cookieParser());
